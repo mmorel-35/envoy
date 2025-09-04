69a04bfd6047e151ab1e36955272503b52dbc5f6
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-09-04 22:59:42 +0200

    Move propagator constants infrastructure to separate extension with unified organization and enhanced constant reuse
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

---
+++ source/extensions/propagators/b3/propagator.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+
+#include "envoy/http/header_map.h"
+
+#include "source/common/singleton/const_singleton.h"
+#include "source/common/tracing/trace_context_impl.h"
+#include "source/extensions/propagators/b3/trace_context.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Propagators {
+namespace B3 {
+
+/**
+ * B3 Trace Propagation constants for header names as defined in:
+ * https://github.com/openzipkin/b3-propagation
+ */
+class B3ConstantValues {
+public:
+  // B3 multi-header format headers
+  const Tracing::TraceContextHandler X_B3_TRACE_ID{std::string(Constants::kTraceIdHeader)};
+  const Tracing::TraceContextHandler X_B3_SPAN_ID{std::string(Constants::kSpanIdHeader)};
+  const Tracing::TraceContextHandler X_B3_PARENT_SPAN_ID{
+      std::string(Constants::kParentSpanIdHeader)};
+  const Tracing::TraceContextHandler X_B3_SAMPLED{std::string(Constants::kSampledHeader)};
+  const Tracing::TraceContextHandler X_B3_FLAGS{std::string(Constants::kFlagsHeader)};
+
+  // B3 single header format
+  const Tracing::TraceContextHandler B3{std::string(Constants::kB3Header)};
+};
+
+using B3Constants = ConstSingleton<B3ConstantValues>;
+
+} // namespace B3
+} // namespace Propagators
+} // namespace Extensions
+} // namespace Envoy
---
+++ source/extensions/propagators/w3c/trace_context.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include "absl/strings/string_view.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace Propagators {
+namespace W3c {
+
+/**
+ * W3C Trace Context specification constants.
+ * See https://www.w3.org/TR/trace-context/
+ */
+namespace Constants {
+// W3C traceparent header format: version-trace-id-parent-id-trace-flags
+constexpr int kTraceparentHeaderSize = 55; // 2 + 1 + 32 + 1 + 16 + 1 + 2
+constexpr int kVersionSize = 2;
+constexpr int kTraceIdSize = 32;
+constexpr int kParentIdSize = 16;
+constexpr int kTraceFlagsSize = 2;
+
+// Header names as defined in W3C specification
+constexpr absl::string_view kTraceparentHeader = "traceparent";
+constexpr absl::string_view kTracestateHeader = "tracestate";
+constexpr absl::string_view kBaggageHeader = "baggage";
+
+// Current version
+constexpr absl::string_view kCurrentVersion = "00";
+
+// Trace flags
+constexpr uint8_t kSampledFlag = 0x01;
+
+// W3C Baggage specification limits
+// See https://www.w3.org/TR/baggage/
+constexpr size_t kMaxBaggageSize = 8192;   // 8KB total size limit
+constexpr size_t kMaxBaggageMembers = 180; // Practical limit to prevent abuse
+constexpr size_t kMaxKeyLength = 256;
+constexpr size_t kMaxValueLength = 4096;
+} // namespace Constants
+
+} // namespace W3c
+} // namespace Propagators
+} // namespace Extensions
+} // namespace Envoy
