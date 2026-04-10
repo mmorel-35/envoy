5aa867cf6a6812037ca3f2af646f56b3b8084298
Matthieu MOREL <matthieu.morel35@gmail.com>
2026-04-10 16:59:23 +0200

    Centralize shared OpenTelemetry code into `source/extensions/common/opentelemetry/`
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

---
+++ source/extensions/common/opentelemetry/exporters/otlp/populate_attribute_utils.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+#include "opentelemetry/proto/common/v1/common.pb.h"
+#include "source/extensions/common/opentelemetry/types.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace OpenTelemetry {
+namespace Exporters {
+namespace Otlp {
+
+class PopulateAttributeUtils {
+public:
+  static void populateAnyValue(opentelemetry::proto::common::v1::AnyValue& value_proto, const OTelAttribute& attribute_value);
+  static KeyValue makeKeyValue(const std::string& key, const std::string& value);
+};
+
+} // namespace Otlp
+} // namespace Exporters
+} // namespace OpenTelemetry
+} // namespace Extensions
+} // namespace Envoy
